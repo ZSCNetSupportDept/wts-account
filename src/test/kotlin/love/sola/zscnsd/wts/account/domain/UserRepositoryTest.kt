ee85e56282bd18b4c0d6fcb8dfe7682f60598fa3
Sola <dev@sola.love>
2018-01-10 14:20:32 +0800

    add UserRepository test
    Signed-off-by: Sola <dev@sola.love>

---
+++ src/test/kotlin/love/sola/zscnsd/wts/account/domain/UserRepositoryTest.kt
@@ -0,0 +1,37 @@
+package love.sola.zscnsd.wts.account.domain
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.junit4.SpringRunner
+
+
+@RunWith(SpringRunner::class)
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Test
+    fun `UserRepository should be able to load Operators`() {
+        val mockOperator = Operator(
+            1501,
+            null,
+            null,
+            emptyList(),
+            2015001010101,
+            "Sola",
+            null,
+            null,
+            null,
+            null
+        )
+        userRepository.save(mockOperator)
+        val loadedUser = userRepository.findById(2015001010101).get()
+        Assert.assertTrue(loadedUser is Operator)
+    }
+
+}

ea00a907e8aafc3939cffa8e04a83f7a51b4b871
Sola <dev@sola.love>
2018-01-18 02:49:00 +0800

    implement convertUserAuthentication
    Signed-off-by: Sola <dev@sola.love>

--- src/main/kotlin/love/sola/zscnsd/wts/account/config/CustomUserAuthenticationConverter.kt
+++ src/main/kotlin/love/sola/zscnsd/wts/account/config/CustomUserAuthenticationConverter.kt
@@ -1,5 +1,7 @@
 package love.sola.zscnsd.wts.account.config
 
+import love.sola.zscnsd.wts.account.domain.Operator
+import love.sola.zscnsd.wts.account.domain.User
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.provider.token.UserAuthenticationConverter
 import org.springframework.stereotype.Component
@@ -7,12 +9,25 @@ import org.springframework.stereotype.Component
 @Component
 class CustomUserAuthenticationConverter : UserAuthenticationConverter {
 
-    override fun extractAuthentication(map: MutableMap<String, *>): Authentication? {
+    override fun extractAuthentication(map: Map<String, *>): Authentication? {
         TODO()
     }
 
-    override fun convertUserAuthentication(userAuthentication: Authentication): MutableMap<String, *> {
-        TODO()
+    override fun convertUserAuthentication(userAuthentication: Authentication): Map<String, *> {
+        val user = userAuthentication.principal as? User
+                ?: throw IllegalArgumentException("Authentication's principal should be User.")
+        val response = hashMapOf<String, Any?>()
+        response["id"] = user.id
+        response["name"] = user.name
+        response["address"] = user.address
+        response["phone"] = user.phone
+        response["account"] = user.account
+        if (user is Operator) {
+            response["stuff_id"] = user.stuffId
+            response["arrangement"] = user.arrangement
+            response["permissions"] = user.permissions
+        }
+        return response
     }
 
 }

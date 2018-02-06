7e64e403ab0ab7bb43726a152be17064f01480ec
Sola <dev@sola.love>
2018-02-06 14:29:05 +0800

    fix start up issue
    Signed-off-by: Sola <dev@sola.love>

--- src/main/kotlin/love/sola/zscnsd/wts/account/domain/UserRepository.kt
+++ src/main/kotlin/love/sola/zscnsd/wts/account/domain/UserRepository.kt
@@ -4,7 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository : JpaRepository<User, Long> {
-    fun findByWechat(wechat: String): User?
-}
+interface UserRepository : JpaRepository<User, Long>
 

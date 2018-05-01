e9d98f7a7694b2c70dc9051aa2a37b691f81b2f6
Sola <dev@sola.love>
2018-05-02 03:06:54 +0800

    add missed @EnableResourceServer annotation
    Signed-off-by: Sola <dev@sola.love>

--- src/main/kotlin/love/sola/zscnsd/wts/account/config/OAuth2ClientConfig.kt
+++ src/main/kotlin/love/sola/zscnsd/wts/account/config/OAuth2ClientConfig.kt
@@ -3,12 +3,14 @@ package love.sola.zscnsd.wts.account.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices
 import org.springframework.security.oauth2.provider.token.TokenStore
 
 @Configuration
+@EnableResourceServer
 class OAuth2ClientConfig(val tokenStore: TokenStore) : ResourceServerConfigurerAdapter() {
 
     @Bean

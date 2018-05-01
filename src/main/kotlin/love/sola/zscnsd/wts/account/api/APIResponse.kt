4e9503cd986037b1bba6abb0ffd93bbe675e4343
Sola <dev@sola.love>
2018-05-02 03:05:52 +0800

    api responses domain structure
    Signed-off-by: Sola <dev@sola.love>

---
+++ src/main/kotlin/love/sola/zscnsd/wts/account/api/APIResponse.kt
@@ -0,0 +1,33 @@
+package love.sola.zscnsd.wts.account.api
+
+class APIResponse {
+
+    companion object {
+        val EMPTY = APIResponse(Any())
+    }
+
+    val data: Any?
+    val error: APIError?
+    val meta: Any?
+
+    constructor(data: Any, meta: Any? = null) {
+        this.data = data
+        this.error = null
+        this.meta = meta
+    }
+
+    constructor(error: APIError, meta: Any? = null) {
+        this.data = null
+        this.error = error
+        this.meta = meta
+    }
+
+}
+
+class APIError(val code: Int, val title: String, val detail: String? = title, val meta: Any? = null) {
+    companion object {
+        val ILLEGAL_USER_INPUT = APIError(40001, "illegal user input")
+    }
+
+    fun withDetail(detail: String) = APIError(code, title, detail, meta)
+}
